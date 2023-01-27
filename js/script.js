e5679ff3d5e8e5c0c01b0bc9d96d25562f7514cd
ELZ <aelzahi@gmail.com>
2023-01-27 06:57:13 -0800

    Add initial USD-Page files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+setTimeout(function(){
+    $('.loader-bg').fadeToggle();
+}, 1500);
+
+function bigImg(x) {
+    x.style.opacity = "0";
+  }
+  function normalImg(x) {
+    x.style.opacity = "1";
+  }
+
+// document.getElementById('menu').addEventListener('click', function (){
+//     document.querySelector("#navMen").classList.toggle("naVmen");
+// });
+
+// document.getElementById('contentt').addEventListener('click', function (){
+//     document.querySelector("#navMen").classList.toggle("naVmen");
+// });
+
+// document.getElementById('aboutt').addEventListener('click', function (){
+//     document.querySelector("#navMen").classList.toggle("naVmen");
+// });
+
+
+
+
+
+
