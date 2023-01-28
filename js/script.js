c7e1a1d93cff03f6eccf40b41e029873a1fba9b7
ELZ <aelzahi@gmail.com>
2023-01-28 11:11:58 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,28 @@ setTimeout(function(){
     $('.loader-bg').fadeToggle();
 }, 1500);
 
-function bigImg(x) {
-    x.style.opacity = "0";
-  }
-  function normalImg(x) {
-    x.style.opacity = "1";
-  }
+let img = document.querySelectorAll(".im");
+let numImg = document.querySelectorAll(".li").length;
+
+for (let i = 0 ; i < numImg ; i++ ){
+  document.querySelectorAll(".lii")[i].addEventListener('mouseover', function(){
+    img[i].style.opacity = "0";
+  });
+  document.querySelectorAll(".lii")[i].addEventListener('mouseout', function(){
+    img[i].style.opacity = "1";
+  });
+
+}
+
+
+
+
+// function bigImg(x) {
+//     x.style.opacity = "0";
+//   }
+//   function normalImg(x) {
+//     x.style.opacity = "1";
+//   }
 
 // document.getElementById('menu').addEventListener('click', function (){
 //     document.querySelector("#navMen").classList.toggle("naVmen");
